d47b9811b737b9e1526cd756d21ca6d9c7dfccfd
Bohdan Myslyvchuk <myslyv4uk@gmail.com>
2021-11-12 15:08:34 +0200

    fixed flow with adding thread sleep by enabling to accamulate records for batch read.

--- kafka-consumer-elasticsearch/src/main/java/com/myslyv4uk/kafka/elasticsearch/ElasticSearchConsumer.java
+++ kafka-consumer-elasticsearch/src/main/java/com/myslyv4uk/kafka/elasticsearch/ElasticSearchConsumer.java
@@ -24,6 +24,51 @@ import java.util.Properties;
 @Slf4j
 public class ElasticSearchConsumer {
 	
+	public static void main(String[] args) throws IOException {
+		RestHighLevelClient client = createClient();
+		
+		KafkaConsumer<String, String> esConsumer = createConsumer();
+		while (true) {
+			final ConsumerRecords<String, String> records = esConsumer.poll(Duration.ofMillis(100));
+			log.info("Received {} records.", records.count());
+			
+			for (ConsumerRecord<String, String> record : records) {
+				// log.info("Key: {} Value: {}", record.key(), record.value());
+				//log.info("Partition: {} Offset: {}", record.partition(), record.offset());
+				//possible solution with id (Kafka generic id)
+				//String id =  record.topic() + "_" + record.partition() + "_" + record.offset();
+				String id = extractIdFromTweet(record.value());
+				
+				//insert into ES
+				IndexRequest indexRequest = new IndexRequest(
+								//index which was created by PUT localhost:9200/tweets
+								"twitter",
+								"tweets",
+								id // to make our consumer idempotent
+				).source(record.value(), XContentType.JSON);
+				IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+				log.info(indexResponse.getId());
+				// /twitter/tweets/{id}gIWx0XwBWCBIynXU2gaD
+				
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			log.info("Committing the offsets ...");
+			esConsumer.commitSync();
+			log.info("Offsets have been committed ...");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		//client.close();
+	}
+	
 	public static RestHighLevelClient createClient() {
 //        for BONSAI
 //        String hostName = "url-without-protocol-and-port";
@@ -44,37 +89,6 @@ public class ElasticSearchConsumer {
 		return new RestHighLevelClient(builder);
 	}
 	
-	public static void main(String[] args) throws IOException {
-		RestHighLevelClient client = createClient();
-		
-		
-		KafkaConsumer<String, String> esConsumer = createConsumer();
-		while (true) {
-			final ConsumerRecords<String, String> records = esConsumer.poll(Duration.ofMillis(100));
-			for (ConsumerRecord<String, String> record : records) {
-				// log.info("Key: {} Value: {}", record.key(), record.value());
-				//log.info("Partition: {} Offset: {}", record.partition(), record.offset());
-				//possible solution with id (Kafka generic id)
-				//String id =  record.topic() + "_" + record.partition() + "_" + record.offset();
-				
-				String id = extractIdFromTweet(record.value());
-				
-				//insert into ES
-				IndexRequest indexRequest = new IndexRequest(
-								//index which was created by PUT localhost:9200/tweets
-								"twitter",
-								"tweets",
-								id // to make our consumer idempotent
-				).source(record.value(), XContentType.JSON);
-				IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
-				log.info(indexResponse.getId());
-				// /twitter/tweets/{id}gIWx0XwBWCBIynXU2gaD
-			}
-		}
-		
-		//client.close();
-	}
-	
 	private static String extractIdFromTweet(String value) {
 		return new JsonParser().parse(value)
 						.getAsJsonObject()
@@ -83,13 +97,17 @@ public class ElasticSearchConsumer {
 	}
 	
 	public static KafkaConsumer<String, String> createConsumer() {
+		final String groupId = "es-group";
 		
 		Properties properties = new Properties();
 		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "es-group");
+		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
 		properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false"); //disable autocommit
+		properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "10");
+		
 		
 		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
 		
