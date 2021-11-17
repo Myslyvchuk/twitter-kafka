efad5a62f41089a7782f2663c23fe963fe6ba970
Bohdan Myslyvchuk <myslyv4uk@gmail.com>
2021-11-17 17:55:41 +0200

    added flink module

--- kafka-twitter-producer/src/main/java/com/myslyv4uk/kafka/twittert/TwitterProducer.java
+++ kafka-twitter-producer/src/main/java/com/myslyv4uk/kafka/twittert/TwitterProducer.java
@@ -24,76 +24,75 @@ import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class TwitterProducer {
-
-    private final String consumerKey = "-";
-    private final String consumerSecret = "-";
-    private final String token = "-";
-    private final String secret = "-";
-    private List<String> terms = Lists.newArrayList("bitcoin", "usa", "politics");
-
-    public TwitterProducer() {
-    }
-
-    public static void main(String[] args) {
-        //twitter client
-        new TwitterProducer().run();
-    }
-
-    private void run() {
-        log.info("Set up!");
-        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<>(100000);
-        Client client = createTwitterClient(msgQueue);
-        client.connect();
-
-        KafkaProducer<String, String> producer = createProducer();
-
-
-        while(!client.isDone()){
-            String msg = null;
-            try {
-                 msg = msgQueue.poll(5, TimeUnit.SECONDS);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                client.stop();
-            }
-            if (msg != null) {
-                log.info("{}",msg);
-                producer.send(new ProducerRecord<>("twitter", null, msg), (recordMetadata, e) -> {
-                    if(e != null) {
-                        log.error("Something bad happened", e);
-                    }
-                });
-            }
-        }
-        log.info("End of app!");
-    }
-
-    private KafkaProducer<String, String> createProducer() {
-        Properties properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
-        return new KafkaProducer<>(properties) ;
-    }
-
-    private Client createTwitterClient(BlockingQueue<String> msgQueue) {
-        /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
-        Hosts twitterKafkaHosts = new HttpHosts(Constants.STREAM_HOST);
-        StatusesFilterEndpoint tiwtterKafkaEndpoint = new StatusesFilterEndpoint();
-        // Optional: set up some followings and track terms
-
-        tiwtterKafkaEndpoint.trackTerms(terms);
-
-        Authentication twitterKafkaAuth = new OAuth1(consumerKey, consumerSecret, token, secret);
-
-        ClientBuilder builder = new ClientBuilder()
-                .name("Kafka-Twitter-Client-01")                      // optional: mainly for the logs
-                .hosts(twitterKafkaHosts)
-                .authentication(twitterKafkaAuth)
-                .endpoint(tiwtterKafkaEndpoint)
-                .processor(new StringDelimitedProcessor(msgQueue));  // optional: use this if you want to process client events
-
-        return builder.build();
-    }
+	
+	private final String consumerKey = "i8n9JTXZztCkvYFfEhi7A8wWv";
+	private final String consumerSecret = "s0tha9FA0V0TlAf8ifpM1MkyIW8jXvxGS3FfSpW6Tz9GFPrkng";
+	private final String token = "808051021922635776-9ELWG84dQQx6VpC9wA3szXhBMzx1wC7";
+	private final String secret = "Hn4vWHu688BipLGuBnJgRJyrc0skYTZlZMjy93nwjv6yT";
+	private List<String> terms = Lists.newArrayList("bitcoin");
+	
+	public TwitterProducer() {
+	}
+	
+	public static void main(String[] args) {
+		//twitter client
+		new TwitterProducer().run();
+	}
+	
+	private void run() {
+		log.info("Set up!");
+		BlockingQueue<String> msgQueue = new LinkedBlockingQueue<>(100000);
+		Client client = createTwitterClient(msgQueue);
+		client.connect();
+		
+		KafkaProducer<String, String> producer = createProducer();
+		
+		while (!client.isDone()) {
+			String msg = null;
+			try {
+				msg = msgQueue.poll(5, TimeUnit.SECONDS);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				client.stop();
+			}
+			if (msg != null) {
+				log.info("{}", msg);
+				producer.send(new ProducerRecord<>("twitter", null, msg), (recordMetadata, e) -> {
+					if (e != null) {
+						log.error("Something bad happened", e);
+					}
+				});
+			}
+		}
+		log.info("End of app!");
+	}
+	
+	private KafkaProducer<String, String> createProducer() {
+		Properties properties = new Properties();
+		properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		
+		return new KafkaProducer<>(properties);
+	}
+	
+	private Client createTwitterClient(BlockingQueue<String> msgQueue) {
+		/** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
+		Hosts twitterKafkaHosts = new HttpHosts(Constants.STREAM_HOST);
+		StatusesFilterEndpoint tiwtterKafkaEndpoint = new StatusesFilterEndpoint();
+		// Optional: set up some followings and track terms
+		
+		tiwtterKafkaEndpoint.trackTerms(terms);
+		
+		Authentication twitterKafkaAuth = new OAuth1(consumerKey, consumerSecret, token, secret);
+		
+		ClientBuilder builder = new ClientBuilder()
+						.name("Kafka-Twitter-Client-01")                      // optional: mainly for the logs
+						.hosts(twitterKafkaHosts)
+						.authentication(twitterKafkaAuth)
+						.endpoint(tiwtterKafkaEndpoint)
+						.processor(new StringDelimitedProcessor(msgQueue));  // optional: use this if you want to process client events
+		
+		return builder.build();
+	}
 }
