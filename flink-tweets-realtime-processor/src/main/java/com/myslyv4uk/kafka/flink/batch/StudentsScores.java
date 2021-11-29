11645d22ffb7d5c5d6c3072a0ca702e91d4ad885
Bohdan Myslyvchuk <myslyv4uk@gmail.com>
2021-11-29 18:17:27 +0200

    added average total score and highest score

--- flink-tweets-realtime-processor/src/main/java/com/myslyv4uk/kafka/flink/batch/StudentsScores.java
+++ flink-tweets-realtime-processor/src/main/java/com/myslyv4uk/kafka/flink/batch/StudentsScores.java
@@ -3,7 +3,10 @@ package com.myslyv4uk.kafka.flink.batch;
 import com.myslyv4uk.kafka.flink.util.Util;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.tuple.Tuple4;
 
@@ -17,23 +20,27 @@ public class StudentsScores {
 		 *                     Total Score
 		 ****************************************************************************/
 		//Read students list
-		DataSet<Tuple4<String, String, Double, Double>> studentsList =
+		DataSet<Tuple4<String, String, Double, Double>> computedTotalScore =
 						Util.ENV.readCsvFile("flink-tweets-realtime-processor/src/main/resources/student_scores.csv")
 										.ignoreFirstLine()
 										.parseQuotedStrings('\"')
 										.types(String.class, String.class, Double.class,Double.class);
 		
 		System.out.println("First 5 students");
-		studentsList.first(5).print();
+		computedTotalScore.first(5).print();
 		
 		/****************************************************************************
 		 *            1. Compute Total Score for each student and subject
 		 ****************************************************************************/
-		DataSet<Tuple3<String, String, Double>> computedTotalScores = studentsList
-						.map(new MapFunction<Tuple4<String, String, Double, Double>, Tuple3<String, String, Double>>() {
+		DataSet<Tuple4<String, String, Integer, Double>> computedTotalScores = computedTotalScore
+						.map(new MapFunction<Tuple4<String, String, Double, Double>, Tuple4<String, String, Integer, Double>>() {
 							@Override
-							public Tuple3<String, String, Double> map(Tuple4<String, String, Double, Double> studentList) throws Exception {
-								return new Tuple3<>(studentList.f0, studentList.f1, studentList.f2 + studentList.f3);
+							public Tuple4<String, String, Integer, Double> map(Tuple4<String, String, Double, Double> studentList) {
+								return new Tuple4<>(
+												studentList.f0,
+												studentList.f1,
+												1, // acc for grouping
+												studentList.f2 + studentList.f3);
 							}
 						});
 		
@@ -41,12 +48,12 @@ public class StudentsScores {
 		computedTotalScores.first(5).print();
 		
 		/****************************************************************************
-		 *                 Filter Subjects, get only physics
+		 *                 2.Filter Subjects, get only physics
 		 ****************************************************************************/
-		DataSet<Tuple3<String, String, Double>> filteredPhysics = computedTotalScores.
-						filter(new FilterFunction<Tuple3<String, String, Double>>() {
+		DataSet<Tuple4<String, String, Integer, Double>> filteredPhysics = computedTotalScores
+						.filter(new FilterFunction<Tuple4<String, String, Integer, Double>>() {
 							@Override
-							public boolean filter(Tuple3<String, String, Double> computedTotal) {
+							public boolean filter(Tuple4<String, String, Integer, Double> computedTotal) {
 								return computedTotal.f1.equals("Physics");
 							}
 						});
@@ -56,6 +63,45 @@ public class StudentsScores {
 		
 		System.out.println("\nTotal students who learnt physics = " + filteredPhysics.count());
 		
+		/****************************************************************************
+		 *               3.Compute Average Score by Students across Subjects
+		 ****************************************************************************/
+		DataSet<Tuple2<String, Double>> averageTotal = computedTotalScores.<Tuple3<String, Integer, Double>>
+										project(0, 2, 3)
+						.groupBy(0)
+						.reduce(new ReduceFunction<Tuple3<String, Integer, Double>>() {
+							@Override
+							public Tuple3<String, Integer, Double> reduce(
+											Tuple3<String, Integer, Double> rec1,
+											Tuple3<String, Integer, Double> rec2) {
+								return new Tuple3<>(rec1.f0, rec1.f1 + rec2.f1, rec1.f2 + rec2.f2);
+							}
+						})
+						.map(i -> Tuple2.of(i.f0, i.f2 / i.f1))
+						.returns(Types.TUPLE(Types.STRING, Types.DOUBLE));  //Summarize by Product
+		
+		Util.printHeader("Average total score for each student across all subjects");
+		averageTotal.print();
+		
+		/****************************************************************************
+		 *          4. Find Top Student by Subject (maximum total score )
+		 ****************************************************************************/
+		DataSet<Tuple3<String,String, Double>> highestTotalScorePerSubject = computedTotalScore
+						.<Tuple3<String, String, Double>>project(0,1,3)
+						.groupBy(1)
+						.reduce(new ReduceFunction<Tuple3<String, String, Double>>() {
+							@Override
+							public Tuple3<String, String, Double> reduce(
+											Tuple3<String, String, Double> rec1,
+											Tuple3<String, String, Double> rec2) {
+								return (rec1.f2 > rec2.f2)? rec1 : rec2;
+							}
+						});
+		
+		Util.printHeader("Top Student by Subject");
+		highestTotalScorePerSubject
+						.project(1,0,2) //Rearrange Tuple so Subject comes first
+						.print();
 		
 	}
 }
